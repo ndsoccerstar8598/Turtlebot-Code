4cd0d27faaf0196938b227201f314d9d3c7595c6
ndsoccerstar8598 <ndsoccerstar55@gmail.com>
2017-06-15 14:51:42 -0400

    Add files via upload
    make while loop greater than or equal

--- MedicationAlertTimed.py
+++ MedicationAlertTimed.py
@@ -86,7 +86,7 @@ if __name__ == '__main__':
         rospy.loginfo("Go to (%s, %s) pose", position['x'], position['y'])
         now = datetime.datetime.now()
 	when = now.replace(hour=now.hour, minute=now.minute+1, second=0, microsecond=0)
-        while (when != datetime.datetime.now()):
+        while (when >= datetime.datetime.now()):
 		rospy.sleep(1)
         success = navigator.goto(position, quaternion)
         success.navigator.wait_for_result(rospy.Duration(240))
