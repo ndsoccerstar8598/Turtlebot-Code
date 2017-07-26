80a32a2177145c953de412c1e5a1e8bbdc8be298
kristentan1 <ktan1@stevens.edu>
2017-07-26 14:02:05 -0400

    Add files via upload

--- MedicationAlert3.py
+++ MedicationAlert3.py
@@ -83,6 +83,13 @@ class issueReminder():
 
 if __name__ == '__main__':
     try:
+        def __init__(self):
+            rospy.on_shutdown(self.shutdown)
+        
+        def shutdown(self):
+            rospy.loginfo("Stop")
+            rospy.sleep(1)
+        
         def reminder(self):
             self.soundhandle = SoundClient()
             rospy.sleep(1)
